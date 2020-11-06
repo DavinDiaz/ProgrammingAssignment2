f149058766ba78082c9d3dc3acb5ce5527448116
Davin Diaz <d.diazg7@gmail.com>
2020-11-05 20:07:58 -0600

    Commiting changes in cachematrxi.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create a special square matrix and 
+##caches its inverted version
 
-## Write a short comment describing this function
+## The first function creates a special matrix, which in turn is just a list
+## that sets the value of the matrix, gets the value of the matrix, set the
+## value of the inverted matrix and gets the value of the inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function calcualtes the inverted matrix of the special matrix created
+## with the function above
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+  }
